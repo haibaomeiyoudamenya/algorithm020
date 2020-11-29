ecd99a9a2180826d186fc7400397cc8c07af8daf
haibaomeiyoudamenya <382408496@qq.com>
2020-11-29 11:11:46 +0800

    Add files via upload
    第二周笔记与作业

---
+++ Week2/[242]有效的字母异位词.java
@@ -0,0 +1,44 @@
+package leetcode.editor.cn;
+//给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。 
+//
+// 示例 1: 
+//
+// 输入: s = "anagram", t = "nagaram"
+//输出: true
+// 
+//
+// 示例 2: 
+//
+// 输入: s = "rat", t = "car"
+//输出: false 
+//
+// 说明: 
+//你可以假设字符串只包含小写字母。 
+//
+// 进阶: 
+//如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？ 
+// Related Topics 排序 哈希表 
+// 👍 308 👎 0
+
+
+import org.springframework.util.StringUtils;
+
+import java.util.Arrays;
+
+public class 有效的字母异位词{
+ public static void main(String[] args) {
+    Solution solution = new 有效的字母异位词().new Solution();
+ }
+ //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] sChar = s.toCharArray();
+        char[] tChar = t.toCharArray();
+        Arrays.sort(sChar);
+        Arrays.sort(tChar);
+        return Arrays.equals(sChar,tChar);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
